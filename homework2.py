dedfbf58a26802266f203a6e8861b12c59742ba2
New-Sai <nevidomskiy00@mail.ru>
2024-06-15 23:00:19 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course_name = 'Phyton'
+homework = 12
+hours = 1.5
+hour_for_one_task = hours/homework
+print('Курс:',course_name,'всего задач:',homework,'затрачено часов:',hours,'среднее время выполнения:',hour_for_one_task)
