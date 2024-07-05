1fcb9716bbc2c17d7274541c93feb26df204eeda
New-Sai <nevidomskiy00@mail.ru>
2024-07-05 21:00:46 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,9 +1,9 @@
 def pasword_guessing(n):
     result = ''
-    for i in range(1,n):
-        for j in range(i+1,n):
+    for i in range(1, n):
+        for j in range(i + 1, n):
             if (i + j) % n == 0:
-                result = result + str(i) + result + str(j)
+                result += str(i) + str(j)
     return result
 number = 0
 while number < 3 or number > 20:
